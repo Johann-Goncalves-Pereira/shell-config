153d0672f5ad02d062787968cfbb1928dbbf30ad
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-04-21 19:51:59 -0300

    feat: :ambulance: new things to consider

--- zsh/behaviors.zsh
+++ zsh/behaviors.zsh
@@ -7,3 +7,18 @@ bindkey -M vicmd '^H' backward-kill-word
 bindkey -M emacs '^[[3;5~' kill-word
 bindkey -M viins '^[[3;5~' kill-word
 bindkey -M vicmd '^[[3;5~' kill-word
+
+#? Syntax Highlighting
+source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+#? LSCOLORS
+source ~/.local/share/lscolors.sh
+
+#? Config Auto Jump
+[[ -s /home/johann/.autojump/etc/profile.d/autojump.sh ]] && source /home/johann/.autojump/etc/profile.d/autojump.sh
+autoload -U compinit && compinit -u
+
+#? Config for the colorize plugin
+ZSH_COLORIZE_TOOL=chroma
+ZSH_COLORIZE_STYLE="colorful"
+ZSH_COLORIZE_CHROMA_FORMATTER=terminal256
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -13,6 +13,10 @@ fi
 # Path to your oh-my-zsh installation.
 export ZSH="/home/johann/.oh-my-zsh"
 
+
+#? To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
@@ -30,11 +34,11 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Uncomment the following line to use hyphen-insensitive completion.
 # Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
+HYPHEN_INSENSITIVE="true"
 
 # Uncomment one of the following lines to change the auto-update behavior
 # zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
+zstyle ':omz:update' mode auto      # update automatically without asking
 # zstyle ':omz:update' mode reminder  # just remind me to update when it's time
 
 # Uncomment the following line to change how often to auto-update (in days).
@@ -50,13 +54,13 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 # You can also set it to another string to have that shown instead of the default red dots.
 # e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
 # Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
-# COMPLETION_WAITING_DOTS="true"
+COMPLETION_WAITING_DOTS="true"
 
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
@@ -71,9 +75,6 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # see 'man strftime' for details.
 # HIST_STAMPS="mm/dd/yyyy"
 
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
@@ -81,13 +82,25 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
   sudo 
+  man
   
   git 
   git-extras 
+  git-auto-fetch
+  # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git-flow  
+  git-flow  
+  git-flow-avh
+
+  catimg
+  compleat
+  timer
 
   colorize
-  zsh-completions 
+  colored-man-pages
+
   command-not-found
+  
+  zsh-completions 
   zsh-autosuggestions
 
   aliases
@@ -102,6 +115,7 @@ plugins=(
   node 
   golang 
 
+  coffee
 
   docker 
   flutter
@@ -111,75 +125,30 @@ plugins=(
   ubuntu  
   torrent
   web-search
+
   copypath
   copyfile
+  
   history
 )
 
-emulate sh -c 'source /etc/profile'
 
-# Base of all settings
+#? Base of all settings
+emulate sh -c 'source /etc/profile'
 source $ZSH/oh-my-zsh.sh
 
-# Zsh Manager
-source ~/.zplug/init.zsh
-
-# Syntax Highlighting
-source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# LSCOLORS
-source ~/.local/share/lscolors.sh
-
-# User Config
+#* User Config variable Path
 USER_CONFIG_DIRECTORY="$HOME/.shell-config/zsh"
 
 source $USER_CONFIG_DIRECTORY/aliases.zsh
 source $USER_CONFIG_DIRECTORY/behaviors.zsh
 source $USER_CONFIG_DIRECTORY/colors.zsh
 source $USER_CONFIG_DIRECTORY/fuctions.zsh
+# source $USER_CONFIG_DIRECTORY/lscolor.zsh
 
-# Auto Jump
-[[ -s /home/johann/.autojump/etc/profile.d/autojump.sh ]] && source /home/johann/.autojump/etc/profile.d/autojump.sh
 
-autoload -U compinit && compinit -u
 
-# # append
-# path+=('/home/david/pear/bin')
-# # or prepend
-# path=('/home/david/pear/bin' $path)
-# # export to sub-processes (make it inherited by child processes)
-# export PATH
-
-# typeset -T LD_LIBRARY_PATH ld_library_path :
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-### ZNT's installer added snippet ###
+###? ZNT's installer added snippet ###
 fpath=( "$fpath[@]" "$HOME/.config/znt/zsh-navigation-tools" )
 autoload n-aliases n-cd n-env n-functions n-history n-kill n-list n-list-draw n-list-input n-options n-panelize n-help
 autoload znt-usetty-wrapper znt-history-widget znt-cd-widget znt-kill-widget
@@ -191,32 +160,32 @@ setopt AUTO_PUSHD HIST_IGNORE_DUPS PUSHD_IGNORE_DUPS
 zstyle ':completion::complete:n-kill::bits' matcher 'r:|=** l:|=*'
 ### END ###
 
+#? Zsh Manager
+source ~/.zplug/init.zsh
 
-### Added by Zinit's installer
-if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
-        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+###? Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
 fi
 
-source "$HOME/.zinit/bin/zinit.zsh"
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
-
-
 
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \
-    zinit-zsh/z-a-rust \
-    zinit-zsh/z-a-as-monitor \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-bin-gem-node
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
 
-### End of Zinit's installer chunk
-
+  
+#? flatpak config 
 emulate sh -c 'source /etc/profile.d/apps-bin-path.sh'
 emulate sh -c 'source /etc/profile.d/flatpak.sh'
 
