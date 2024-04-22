d376742f50f7be3bbcece5619c0459a6a1a27f94
Johann Goncalves Pereira <johann.gon.pereira@gmail.com>
2024-04-22 13:16:31 -0300

    feat: wakatime

--- zsh/config/p10k-config.zsh
+++ zsh/config/p10k-config.zsh
@@ -1,7 +1,1 @@
-# > Cache p10k 
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-# > To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
+../../../.dotfile/.shell-config/zsh/config/p10k-config.zsh
