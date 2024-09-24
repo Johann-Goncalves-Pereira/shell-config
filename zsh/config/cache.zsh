9e28a779c9cec62980f53d0ed9c5c13b30f6699c
Johann Goncalves Pereira <johann.gon.pereira@gmail.com>
2024-09-24 13:10:58 -0300

    feat: new changes 2

--- zsh/config/cache.zsh
+++ zsh/config/cache.zsh
@@ -1,14 +1,1 @@
-# > ------- < #
-# >  Cache  < #
-# > ------- < #
-# Send cache to XDG_CACHE_HOME if set or $HOME/.cache
-export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
-() {
-  emulate -L zsh
-  local -r cache_dir=${XDG_CACHE_HOME:-$HOME/.cache}/zsh
-  autoload -Uz _store_cache compinit
-  zstyle ':completion:*' use-cache true
-  zstyle ':completion:*' cache-path $cache_dir/.zcompcache
-  [[ -f $cache_dir/.zcompcache/.make-cache-dir ]] || _store_cache .make-cache-dir
-  compinit -C -d $cache_dir/.zcompdump
-}
+../../../.dotfile/.shell-config/zsh/config/cache.zsh
